948dfebf174ebaf5f9ff70845157bf09771049d0
Guchetl Murat <gmurka@gmail.com>
2013-02-14 17:51:53 +0300

    initial state

---
+++ satchless/__init__.py
@@ -0,0 +1,8 @@
+'''
+Satchless
+
+The Django e-commerce framework for perfectionists who also care about code
+quality.
+'''
+
+__version__ = '2012.11b'
---
+++ satchless/category/tests/__init__.py
@@ -0,0 +1,3 @@
+from .category import (Views, Models, CategorizedProductUrlTests, category_app)
+
+__all__ = ['Views', 'Models', 'CategorizedProductUrlTests', 'category_app']
