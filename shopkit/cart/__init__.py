5155e39397ae58e350dd8dd249bd3733a401c69a
Guchetl Murat <gmurka@gmail.com>
2015-08-11 15:11:04 +0300

    last original satchless commit merge (162370f2fdb4acf4737c986fde6725aab5ba62da)

--- shopkit/cart/__init__.py
+++ shopkit/cart/__init__.py
@@ -1,9 +1,73 @@
-class InvalidQuantityException(Exception):
+from satchless.item import ItemSet, ItemLine
 
-    def __init__(self, reason, quantity_delta):
-        self.reason = reason
-        self.quantity_delta = quantity_delta
 
-    def __str__(self):
-        return self.reason
+class CartLine(ItemLine):
 
+    def __init__(self, product, quantity, data=None):
+        self.product = product
+        self._quantity = quantity
+        self.data = data
+
+    def __repr__(self):
+        return 'CartLine(product=%r, quantity=%r, data=%r)' % (
+            self.product, self.quantity, self.data)
+
+    def get_quantity(self):
+        return self._quantity
+
+    quantity = property(get_quantity)
+
+    def get_price_per_item(self, **kwargs):
+        return self.product.get_price(**kwargs)
+
+
+class Cart(ItemSet):
+
+    SESSION_KEY = 'cart'
+    modified = False
+
+    state = None
+
+    def __init__(self, items=None):
+        self.state = {}
+        self.modified = True
+        items = items or []
+        for l in items:
+            self.add_item(l.product, l.quantity, l.data, replace=True)
+
+    def __repr__(self):
+        return 'Cart(%r)' % (list(self),)
+
+    def __iter__(self):
+        for key, qty in self.state.iteritems():
+            product, data = key
+            yield CartLine(product, qty, data)
+
+    def __getstate__(self):
+        return self.state
+
+    def __setstate__(self, state):
+        self.state = state
+
+    def count(self):
+        return sum([item.get_quantity() for item in self])
+
+    def check_quantity(self, product, quantity, data=None):
+        return True
+
+    def add_item(self, product, quantity, data=None, replace=False):
+        key = (product, data)
+        if not replace and key in self.state:
+            quantity += self.state[key]
+
+        if quantity < 0:
+            raise ValueError('%r is not a valid quantity' % (quantity,))
+
+        self.check_quantity(product, quantity, data)
+
+        if not quantity and product in self:
+            del self.state[key]
+        elif quantity:
+            self.state[key] = quantity
+
+        self.modified = True
--- shopkit/cart/tests/__init__.py
+++ shopkit/cart/tests/__init__.py
@@ -1,8 +1,33 @@
 from prices import Price
+from django.test import TestCase
+from satchless.item import ItemLine
 
+from ...cart import Cart
 from ...product.tests import product_app
 from ...util.models import construct
 from .. import models
+
+
+class Product(ItemLine):
+
+    def get_price_per_item(self, **kwargs):
+        return Price(1)
+
+
+class TestSessionCart(TestCase):
+
+    def test_add_item(self):
+        cart = Cart()
+        cart.add_item('shrubbery', 0)
+        self.assertEqual(cart.count(), 0)
+        cart.add_item('shrubbery', 1)
+        self.assertEqual(cart.count(), 1)
+        cart.add_item('shrubbery', 2)
+        self.assertEqual(cart.count(), 3)
+        cart.add_item('shrubbery', 10, replace=True)
+        self.assertEqual(cart.count(), 10)
+        cart.add_item('shrubbery', 10, data='trimmed', replace=True)
+        self.assertEqual(cart.count(), 20)
 
 
 class TestCart(models.Cart):
@@ -17,4 +42,5 @@ from .app import AppTestCase
 from .magic_app import MagicAppTestCase, cart_app
 from .models import ModelsTestCase
 
-__all__ = ['AppTestCase', 'cart_app', 'MagicAppTestCase', 'ModelsTestCase']
+__all__ = ['AppTestCase', 'cart_app', 'MagicAppTestCase', 'ModelsTestCase',
+           'CartTest']
